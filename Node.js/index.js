a2120d0b6e36c10c3ba5348dc88a65b7f3e8df99
JoeRyan4 <56027134+JoeRyan4@users.noreply.github.com>
2020-04-09 14:47:07 +0100

    Update index.js

--- Node.js/index.js
+++ Node.js/index.js
@@ -90,7 +90,46 @@ server.listen(port, () => {
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+const path = require('path')
+const http = require('http')
+const express = require('express')
+const socketio = require('socket.io')
 
+const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
+
+const port = process.env.PORT || 3000
+const publicDirectoryPath = path.join(__dirname, '../public')
+
+app.use(express.static(publicDirectoryPath))
+
+// //Goal: Allow Clients to send message
+// //1. create a form with an input and button
+// 2.set up a listener to form submisssions 
+// 3.Have server listen for "sendMesages"
+// 4. Test code
+
+
+io.on('connection', (socket) => {
+    console.log('New Websocket connection')
+
+    socket.emit('message', 'welcome')
+
+    // socket.emit('countUpdated', count)
+
+
+    // socket.on('increment', () => {
+    //     count++
+    //     //socket.emit('countUpdated', count)
+    //     io.emit('countUpdated', count)
+   // })
+})
+
+
+server.listen(port, () => {
+    console.log(`Server is up on port ${port}!`)
+})
 
 
 
