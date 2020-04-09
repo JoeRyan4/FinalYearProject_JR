437ee4103b47a75c5113b3a2787699332247df18
JoeRyan4 <56027134+JoeRyan4@users.noreply.github.com>
2020-04-09 14:40:49 +0100

    Update index.js

--- Node.js/index.js
+++ Node.js/index.js
@@ -47,13 +47,48 @@ server.listen(port, () => {
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
-1.Send a welcome message to new users ie.(send welcome as an event data
-2.Have the client listen for a message event and print the message to complete.
-3. Test code
+// 1.Send a welcome message to new users ie.(send welcome as an event data
+// 2.Have the client listen for a message event and print the message to complete.
+// 3.Test code
+
+
+const path = require('path')
+const http = require('http')
+const express = require('express')
+const socketio = require('socket.io')
+
+const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
+
+const port = process.env.PORT || 3000
+const publicDirectoryPath = path.join(__dirname, '../public')
+
+app.use(express.static(publicDirectoryPath))
 
 
 
+io.on('connection', (socket) => {
+    console.log('New Websocket connection')
 
+    socket.emit('message', 'welcome')
+
+    // socket.emit('countUpdated', count)
+
+
+    // socket.on('increment', () => {
+    //     count++
+    //     //socket.emit('countUpdated', count)
+    //     io.emit('countUpdated', count)
+   // })
+})
+
+
+server.listen(port, () => {
+    console.log(`Server is up on port ${port}!`)
+})
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
 
