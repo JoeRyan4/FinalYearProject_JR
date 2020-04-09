b40755ee71f2d900b56f02b6ee4a4590ea8f0d31
JoeRyan4 <56027134+JoeRyan4@users.noreply.github.com>
2020-04-09 15:31:58 +0100

    Update chat.js

--- Node.js/chat.js
+++ Node.js/chat.js
@@ -54,6 +54,38 @@ document.querySelector('#message-form').addEventListener('submit', (e) => {
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+const socket = io()
+
+
+socket.on('message', (message) => {
+    console.log(message)
+})
+
+document.querySelector('#message-form').addEventListener('submit', (e) => {
+
+        e.preventDefault()
+
+    //target represents the the target event we are listening i.e 'form'  
+    //Alternative way to get the input, less likely to crash when the HTML is being altered
+     const message = e.target.elements.message.value
+     
+     socket.emit('sendMessage', message)
+})
+
+
+document.querySelector('send me-location').addEventListener('click', () => {
+    if (navigator.geolocation) {
+        return alert('Geolocation is not supported by your Browser')
+    }
+
+
+    navigator.geolocation.getCurrentPosition((position) => {
+
+            console.log(position)
+    })
+})
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
 
