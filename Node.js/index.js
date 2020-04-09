77d0f0ec8713a214529a474b8b4410bc42baeedf
JoeRyan4 <56027134+JoeRyan4@users.noreply.github.com>
2020-04-09 17:08:22 +0100

    Update index.js

--- Node.js/index.js
+++ Node.js/index.js
@@ -249,7 +249,48 @@ server.listen(port, () => {
 
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+const path = require('path')
+const http = require('http')
+const express = require('express')
+const socketio = require('socket.io')
 
+const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
+
+const port = process.env.PORT || 3000
+const publicDirectoryPath = path.join(__dirname, '../public')
+
+app.use(express.static(publicDirectoryPath))
+
+//function emulates when a user has joined/left the chat gooup
+
+io.on('connection', (socket) => {
+    console.log('New Websocket connection')
+
+    socket.emit('message', 'welcome')
+    socket.broadcast.emit('message', 'new user joined')
+
+    socket.on('sendMessage', (message, collback) => {
+        io.emit('message', message)
+        callback('Delivered')
+    })
+
+    socket.on('sendLocation', (coords) => {
+        io.emit('message', `https://google.com/maps?q=${coords.latitude},${coords.longitude}`)
+    })
+
+    socket.on('disconnect', () => {
+        io.emit('message','a user has left')
+    })
+})
+
+server.listen(port, () => {
+    console.log(`Server is up on port ${port}!`)
+})
 
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
