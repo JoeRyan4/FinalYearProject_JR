91fba658f30845f7dbcd6b5196819caea5dfdd70
JoeRyan4 <56027134+JoeRyan4@users.noreply.github.com>
2020-04-09 14:39:53 +0100

    Update chat.js

--- Node.js/chat.js
+++ Node.js/chat.js
@@ -8,3 +8,32 @@ const socket = io()
 //     console.log('Clicked')
 //     socket.emit('increment')
 // })
+
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+const socket = io()
+
+
+socket.on('message', (message) => {
+    console.log(message)
+})
+
+
+
+// socket.on('countUpdated', (count) => {
+//     console.log('The count has been updated', count)
+// })
+
+// document.querySelector('#increment').addEventListener('click', () => {
+//     console.log('Clicked')
+//     socket.emit('increment')
+// })
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+
