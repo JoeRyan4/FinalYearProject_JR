a1440b0f0f1eea0c38b20b7b5993c501266f7f61
JoeRyan4 <56027134+JoeRyan4@users.noreply.github.com>
2020-04-09 21:49:39 +0100

    Update chat.js

--- Node.js/chat.js
+++ Node.js/chat.js
@@ -310,6 +310,60 @@ document.querySelector('#send-location').addEventListener('click', () => {
     })
 }) 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+const socket = io()
+
+//Elements 
+const $messageForm = document.querySelector('#message-form')
+const $messageFormInput = $messageForm.querySelector('input')
+const $messageFormButton = $messageForm.querySelector('button')
+const $sendLocationButton = document.querySelector('#send-location')
+
+socket.on('message', (message) => {
+    console.log(message)
+})
+
+$messageForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    $messageFormButton.setAttribute('disabled', 'disabled')
+
+    const message = e.target.elements.message.value
+     //disable
+    socket.emit('sendMessage', message, (error) => {
+        $messageFormButton.removeAttribute('disabled')
+        $messageFormInput.value = ''
+        $messageFormInput.focus()
+
+        //enable
+        if(error){
+            return console.log(error)
+        }
+
+        console.log('Message delivered')
+    })
+})
+
+
+$sendLocationButton.addEventListener('click', () => {
+    if (!navigator.geolocation) {
+        return alert('Geolocation is not supported by your Browser')
+    }
+ 
+
+    navigator.geolocation.getCurrentPosition((position) => {
+        socket.emit('sendLocation', {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+     
+        }, () => {
+            console.log('Location shared!')
+        })
+    })
+}) 
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
 
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
