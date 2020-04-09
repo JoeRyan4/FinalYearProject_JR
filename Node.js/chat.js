e6e8954b0c75a2d00e305c1f87c2f65f54e1e784
JoeRyan4 <56027134+JoeRyan4@users.noreply.github.com>
2020-04-09 17:27:53 +0100

    Update chat.js

--- Node.js/chat.js
+++ Node.js/chat.js
@@ -181,6 +181,51 @@ document.querySelector('#send-location').addEventListener('click', () => {
 
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+const socket = io()
+
+
+socket.on('message', (message) => {
+    console.log(message)
+})
+
+document.querySelector('#message-form').addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const message = e.target.elements.message.value
+     
+    socket.emit('sendMessage', message, (error) => {
+        if(error){
+            return console.log(error)
+        }
+
+        console.log('Message delivered')
+    })
+})
+
+
+document.querySelector('#send-location').addEventListener('click', () => {
+    if (!navigator.geolocation) {
+        return alert('Geolocation is not supported by your Browser')
+    }
+ 
+
+    navigator.geolocation.getCurrentPosition((position) => {
+        socket.emit('sendLocation', {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+        })
+    })
+}) 
+
+// ***Acknowledgment Setup**
+
+// 1. Client acknowledgement function
+// 2. server to send back acknowledgement 
+// 3. Client print "location being shared" as acknowledgement
+// 4. Test work
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
