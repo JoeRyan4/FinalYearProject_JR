4f05814fce0d9373a4df933705ee7adc80d3f283
JoeRyan4 <56027134+JoeRyan4@users.noreply.github.com>
2020-04-09 17:34:22 +0100

    Update chat.js

--- Node.js/chat.js
+++ Node.js/chat.js
@@ -225,6 +225,49 @@ document.querySelector('#send-location').addEventListener('click', () => {
 // 4. Test work
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+const socket = io()
+
+
+socket.on('message', (message) => {
+    console.log(message)
+})
+
+document.querySelector('#message-form').addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const message = e.target.elements.message.value
+     
+    socket.emit('sendMessage', message, (error) => {
+        if(error){
+            return console.log(error)
+        }
+
+        console.log('Message delivered')
+    })
+})
+
+
+document.querySelector('#send-location').addEventListener('click', () => {
+    if (!navigator.geolocation) {
+        return alert('Geolocation is not supported by your Browser')
+    }
+ 
+
+    navigator.geolocation.getCurrentPosition((position) => {
+        socket.emit('sendLocation', {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+     
+        }, () => {
+            console.log('Location shared!')
+        })
+    })
+}) 
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
 
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
