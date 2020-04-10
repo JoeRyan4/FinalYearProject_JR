a27e7b5f9e50512ff1d4e586356d5be8e56017cf
JoeRyan4 <56027134+JoeRyan4@users.noreply.github.com>
2020-04-10 17:33:50 +0100

    Update index.js

--- Node.js/index.js
+++ Node.js/index.js
@@ -538,6 +538,63 @@ server.listen(port, () => {
 
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+const path = require('path')
+const http = require('http')
+const express = require('express')
+const socketio = require('socket.io')
+const Filter = require('bad-words')
+const { generateMessage,generateLocationMessage } = require('./utils/messages')
+
+const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
+
+const port = process.env.PORT || 3000
+const publicDirectoryPath = path.join(__dirname, '../public')
+
+app.use(express.static(publicDirectoryPath))
+
+io.on('connection', (socket) => {
+    console.log('New WebSocket connection')
+
+    // socket.emit('message', generateMessage('Welcome!'))
+    // socket.broadcast.emit('message', generateMessage('A new user has joined!'))
+
+    socket.on('join', ({username, room}) => {
+        socket.join(room)
+
+        socket.emit('message', generateMessage('Welcome!'))
+        socket.broadcast.to(room).emit('message', generateMessage('${username} has joined'))
+
+        //socket.emit, io.emit, socket.broadcast.emt
+        //io.to.emit, socket.broadcast.to.emit
+    })
+
+
+    socket.on('sendMessage', (message, callback) => {
+        const filter = new Filter()
+
+        if (filter.isProfane(message)) {
+            return callback('Profanity is not allowed!')
+        }
+
+        io.emit('message', generateMessage(message))
+        callback()
+    })
+
+    socket.on('sendLocation', (coords, callback) => {
+        io.emit('locationMessage', generateLocationMessage())
+        callback()
+    })
+
+    socket.on('disconnect', () => {
+        io.emit('message', generateMessage('A user has left!'))
+    })
+})
+
+server.listen(port, () => {
+    console.log(`Server is up on port ${port}!`)
+})
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
