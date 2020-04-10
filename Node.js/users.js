f8874dd86891fe8e9e64b41a8417df3c123f17ea
JoeRyan4 <56027134+JoeRyan4@users.noreply.github.com>
2020-04-10 19:30:29 +0100

    Create users.js

---
+++ Node.js/users.js
@@ -0,0 +1,46 @@
+const users =[]
+
+const addUser = ({id, username, room }) => {
+
+    //Clean data 
+    username = username.trim().toLowerCase()
+    room = room.trim().toLowerCase()
+
+    //validating the data
+    if(!username || !room) {
+        return {
+            error: 'Username and room ar required'
+        }
+    }
+
+    //Checking for the existing user
+    const existingUser = users.find((user) =>{
+        return user.room === room && user.username === username
+    })
+
+
+
+    //Validate the username 
+    if(existingUser){
+        return {
+            error: 'Username is in use!'
+        }
+    }
+
+    //store the user
+    const user = { id, username, room }
+    users.push(user)
+    return { user }
+}
+
+addUser({
+    id: 23,
+    username: 'Joe',
+    room: 'Lounge'
+})
+
+console.log(users)
+
+//running test in the terminal 'node src/utils/users.js'
+
+
