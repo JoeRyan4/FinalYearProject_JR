7ca5d56f1ef2e2b57c2124c80109fa8c9de74fb8
JoeRyan4 <56027134+JoeRyan4@users.noreply.github.com>
2020-04-11 19:20:33 +0100

    Update users.js

--- Node.js/users.js
+++ Node.js/users.js
@@ -135,7 +135,70 @@ module.exports = {
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+const users =[]
 
+const addUser = ({id, username, room }) => {
+
+    //Clean data 
+    username = username.trim().toLowerCase()
+    room = room.trim().toLowerCase()
+
+    //validating the data
+    if(!username || !room) {
+        return {
+            error: 'Username and room ar required'
+        }
+    }
+
+    //Checking for the existing user
+    const existingUser = users.find((user) =>{
+        return user.room === room && user.username === username
+    })
+
+
+
+    //Validate the username 
+    if(existingUser){
+        return {
+            error: 'Username is in use!'
+        }
+    }
+
+    //store the user
+    const user = { id, username, room }
+    users.push(user)
+    return { user }
+}
+
+const removeUser = (id) => {
+    const index = users.findIndex((user) => user.id === id)
+
+    if (index !== -1){
+        return users.splice(index, 1)[0]
+    }
+}
+
+const getUser = (id) => {
+    return users.find((user) => user.id === id)
+}
+
+
+const getUsersInRoom = (room) => {
+    room = room.trim().toLowerCase()
+    return users.filter((user) => user.room === room)
+}
+
+module.exports = { 
+    addUser, 
+    removeUser,
+    getUser,
+    getUsersInRoom 
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
